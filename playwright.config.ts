3eaceb3900d232570b5eb436a0bfbf6c7dc8dfce
feedmephotons <folx2.0@gmail.com>
2026-06-17 10:15:00 -0400

    Support dynamic port mapping in Playwright configuration to avoid EADDRINUSE errors

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
+const PORT = process.env.PORT || '3009';
+
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
@@ -18,7 +17,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:3009',
+    baseURL: `http://localhost:${PORT}`,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -39,8 +38,8 @@ export default defineConfig({
   ],
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start -- -p 3009',
-    url: 'http://localhost:3009',
+    command: `npm run start -- -p ${PORT}`,
+    url: `http://localhost:${PORT}`,
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000,
   },
